39287bf79c5648aae813ddfadafe95bcf89f3018
WhiteFox0-0 <swayammmm0@gmail.com>
2026-07-04 18:35:50 +0530

    init: initialize rust project

---
+++ target/debug/deps/RustWire-2b8abc4e076d22a0.d
@@ -0,0 +1,5 @@
+/Users/apple/workspace/RustWire/target/debug/deps/RustWire-2b8abc4e076d22a0.d: src/main.rs
+
+/Users/apple/workspace/RustWire/target/debug/deps/libRustWire-2b8abc4e076d22a0.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/RustWire-5a6b3c1da34994c4.d
@@ -0,0 +1,5 @@
+/Users/apple/workspace/RustWire/target/debug/deps/RustWire-5a6b3c1da34994c4.d: src/main.rs
+
+/Users/apple/workspace/RustWire/target/debug/deps/libRustWire-5a6b3c1da34994c4.rmeta: src/main.rs
+
+src/main.rs:
